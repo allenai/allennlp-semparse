75583c92b2b11808364203575922be5b16a95cc2
Brendan Roof <brendanr@allenai.org>
2019-10-23 18:45:42 -0700

    Setup and version

---
+++ setup.py
@@ -0,0 +1,72 @@
+from setuptools import setup, find_packages
+import sys
+import os
+
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# release markers:
+#   X.Y
+#   X.Y.Z   # For bugfix releases
+#
+# pre-release markers:
+#   X.YaN   # Alpha release
+#   X.YbN   # Beta release
+#   X.YrcN  # Release Candidate
+#   X.Y     # Final release
+
+# version.py defines the VERSION and VERSION_SHORT variables.
+# We use exec here so we don't import allennlp_semparse whilst setting up.
+VERSION = {}
+with open("allennlp_semparse/version.py") as version_file:
+    exec(version_file.read(), VERSION)
+
+# Load requirements.txt with a special case for allennlp so we can handle
+# cross-library integration testing.
+with open("requirements.txt") as requirements_file:
+    install_requirements = requirements_file.readlines()
+    install_requirements = [
+        r for r in install_requirements if "git+git://github.com/allenai/allennlp" not in r
+    ]
+    if "EXCLUDE_ALLENNLP_IN_SETUP" not in os.environ:
+        #dependency_links=["http://github.com/allenai/allennlp/tarball/93024e53c1445cb4630ee5c07926abff8943715f#egg={FAKE_VERSION}"],
+        #requirement = "allennlp @ git+ssh://git@github.com/allenai/allennlp@v1.1#egg=some-pkg"
+        requirement = "allennlp @ git+ssh://git@github.com/allenai/allennlp@93024e53c1445cb4630ee5c07926abff8943715f#egg=allennlp"
+        requirement = "allennlp @ git+ssh://git@github.com/allenai/allennlp@457a85bde57ba58289d32e4e1d07df1f94c813b0#egg=allennlp"
+        install_requirements.append(requirement)
+
+# make pytest-runner a conditional requirement,
+# per: https://github.com/pytest-dev/pytest-runner#considerations
+needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
+pytest_runner = ["pytest-runner"] if needs_pytest else []
+
+setup_requirements = [
+    # add other setup requirements as necessary
+] + pytest_runner
+
+setup(
+    name="allennlp_semparse",
+    version=VERSION["VERSION"],
+    description="A framework for building semantic parsers (including neural module networks) with AllenNLP, built by the authors of AllenNLP",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    keywords="allennlp NLP deep learning machine reading semantic parsing parsers",
+    url="https://github.com/allenai/allennlp-semparse",
+    author="Allen Institute for Artificial Intelligence",
+    author_email="allennlp@allenai.org",
+    license="Apache",
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=install_requirements,
+    setup_requires=setup_requirements,
+    tests_require=["pytest", "flaky", "responses>=0.7"],
+    include_package_data=True,
+    python_requires=">=3.6.1",
+    zip_safe=False,
+)
