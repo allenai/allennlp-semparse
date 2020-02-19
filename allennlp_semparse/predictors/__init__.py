e1ff04c1d23ee5dfa1d5d40867a5d5d59f59135d
Dirk Groeneveld <dirkg@allenai.org>
2020-02-18 16:03:47 -0800

    Remove Quarel (#19)

--- allennlp_semparse/predictors/__init__.py
+++ allennlp_semparse/predictors/__init__.py
@@ -1,4 +1,3 @@
 from allennlp_semparse.predictors.atis_parser import AtisParserPredictor
 from allennlp_semparse.predictors.nlvr_parser import NlvrParserPredictor
-from allennlp_semparse.predictors.quarel_parser import QuarelParserPredictor
 from allennlp_semparse.predictors.wikitables_parser import WikiTablesParserPredictor
